6a492f9f759f3089e1a2fbd231f42a1519682443
believe <101379299+Believe0127@users.noreply.github.com>
2026-04-03 12:54:23 +0900

    Add files via upload

---
+++ ExpressionParser/Operator.cpp
@@ -0,0 +1,24 @@
+﻿#include "Operator.hpp"
+
+#ifdef _DEBUG
+const char* ConvertOperatorToString(Operator op) {
+    switch (op) {
+    case Operator::ADD: return "Operator::ADD";
+    case Operator::SUB: return "Operator::SUB";
+    case Operator::MUL: return "Operator::MUL";
+    case Operator::DIV: return "Operator::DIV";
+    case Operator::NOP: return "Operator::NOP";
+    }
+    return "Error";
+}
+#endif
+
+Operator ConvertCharToOperator(char op) {
+    switch (op) {
+    case '+': return Operator::ADD;
+    case '-': return Operator::SUB;
+    case '*': return Operator::MUL;
+    case '/': return Operator::DIV;
+    default : return Operator::NOP;
+    }
+}
---
+++ ExpressionParser/Operator.hpp
@@ -0,0 +1,17 @@
+﻿#ifndef OPERATOR_HPP
+#define OPERATOR_HPP
+
+enum class Operator : unsigned int {
+    ADD
+   ,SUB
+   ,MUL
+   ,DIV
+   ,NOP // No Operations
+};
+
+#ifdef _DEBUG
+const char* ConvertOperatorToString(Operator op);
+#endif
+Operator ConvertCharToOperator(char op);
+
+#endif
